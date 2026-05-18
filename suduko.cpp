76d3fe2d47c537429286d734753e926611c30bb9
MD FAHIM SHAHARIAR <mdfahimshahariarfuad@gmail.com>
2026-05-18 19:29:48 +0600

    Suduko code

---
+++ suduko.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isSafe(vector<vector<char>> &board, int row, int col, char dig)
+    {
+        // horizental
+        for (int j = 0; j < 9; j++)
+        {
+            if (board[row][j] == dig)
+            {
+                return false;
+            }
+        }
+
+        // vertical
+        for (int i = 0; i < 9; i++)
+        {
+            if (board[i][col] == dig)
+            {
+                return false;
+            }
+        }
+
+        // grid
+        int srow = (row / 3) * 3;
+        int scol = (col / 3) * 3;
+
+        for (int i = srow; i <= srow + 2; i++)
+        {
+            for (int j = scol; j <= scol + 2; j++)
+            {
+                if (board[i][j] == dig)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool helper(vector<vector<char>> &board, int row, int col)
+    {
+        if (row == 9)
+        {
+            return true;
+        }
+
+        int nextRow = row, nextCol = col + 1;
+        if (nextCol == 9)
+        {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+
+        if (board[row][col] != '.')
+        {
+            return helper(board, nextRow, nextCol);
+        }
+
+        // place the digit
+        for (char dig = '1'; dig <= '9'; dig++)
+        {
+            if (isSafe(board, row, col, dig))
+            {
+                board[row][col] = dig;
+                if (helper(board, nextRow, nextCol))
+                {
+                    return true;
+                }
+                board[row][col] = '.'; // backtrack
+            }
+        }
+        return false; // no solution found
+    }
+
+    void solveSudoku(vector<vector<char>> &board)
+    {
+        helper(board, 0, 0);
+    }
+};
+
+int main()
+{
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+    Solution sol;
+    sol.solveSudoku(board);
+
+    // Print the solved board
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
